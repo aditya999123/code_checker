b3fa2d3644171b17a3fcef3e9546c54d7bc492ee
Aditya <aditya999123@gmail.com>
2017-03-07 11:18:54 +0530

    nav drawer done

--- home/views.py
+++ home/views.py
@@ -4,26 +4,27 @@ from django.http import HttpResponseRedirect, HttpResponse
 from problems.models import group,problems
 # Create your views here.
 def home(request):
-	list=""
-	list+='<li><a href="/home">Home</a>'
-	list+='<li>CONTEST</li>'
+	list1=""
+	list2=""
+	list3=""
+	list4=""
 	for o in group.objects.filter(type='CONTEST'):
 		if(problems.objects.filter(group=o).count()>0):
-			list+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
-	list+='<li>LAB</li>'
+			list1+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
+	
 	for o in group.objects.filter(type='LAB'):
 		if(problems.objects.filter(group=o).count()>0):
-			list+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
+			list2+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
 
-	list+='<li>PRACTICE</li>'
+	
 	for o in group.objects.filter(type='PRACTICE'):
 		if(problems.objects.filter(group=o).count()>0):
-			list+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
+			list3+='<li><a href="/problems?group_id='+str(o.id)+'">'+o.title+'</a></li>'
 	if request.user.is_authenticated():
-		list+='<li><a href="/logout">Logout</a>'
+		list4+='<a style="color: white;" href="/logout">LOGOUT</a>'
 	else:
-		list+='<li><a href="/login">Login</a>'
-	return render(request,'main.html',{'list':list})
+		list4+='<a style="color: white;" href="/login">LOGIN</a>'
+	return render(request,'main.html',{'list1':list1,'list2':list2,'list3':list3,'list4':list4})
 
 def login_check(request):
     if request.user.is_authenticated():
