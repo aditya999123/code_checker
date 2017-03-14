747780be7e51f743e18057ee72ede102e1cd74f9
Aditya <aditya999123@gmail.com>
2017-03-14 12:54:26 +0530

    keys set

--- home/models.py
+++ home/models.py
@@ -5,14 +5,14 @@ import hashlib
 # Create your models here.
 
 class KEYS_List(models.Model):
-	key=models.CharField(max_length=1200,null=False,blank=False)
+	key=models.CharField(max_length=1200,null=False,blank=False,unique=True)
 	modified= models.DateTimeField(auto_now=True,auto_now_add=False)
 	created= models.DateTimeField(auto_now=False,auto_now_add=True)
 	def __unicode__(self):
 		return str(self.key)
 
 class KEYS_internal(models.Model):
-	key=models.ForeignKey(KEYS_List)
+	key=models.ForeignKey(KEYS_List,to_field='key')
 	value=models.CharField(max_length=1200,null=True,blank=True)
 	modified= models.DateTimeField(auto_now=True,auto_now_add=False)
 	created= models.DateTimeField(auto_now=False,auto_now_add=True)
